93736a9727d9802173321c9104594043003405df
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-03-07 17:57:11 +0200

    project completed!

--- script.js
+++ script.js
@@ -44,19 +44,23 @@
 // }
 // getPosition();
 
+let marker = "";
+
 const main = document.querySelector("main");
 
 let toggle = true;
 
-main.addEventListener("click", function () {
-  if (toggle) {
-    main.style.setProperty("z-index", "2");
-    toggle = false;
-  } else {
-    main.style.setProperty("z-index", "2000");
-    toggle = true;
-  }
-});
+document.addEventListener("DOMContentLoaded", entireProject);
+
+// main.addEventListener("click", function () {
+//   if (toggle) {
+//     main.style.setProperty("z-index", "2");
+//     toggle = false;
+//   } else {
+//     main.style.setProperty("z-index", "2000");
+//     toggle = true;
+//   }
+// });
 
 const dataIP = document.querySelector(".data-ip");
 const dataLocation = document.querySelector(".data-location");
@@ -64,36 +68,50 @@ const dataTimezone = document.querySelector(".data-timezone");
 const dataISP = document.querySelector(".data-isp");
 const svg = document.querySelector("svg");
 
+async function newSearch() {
+  let result;
+  await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_zVKFWWrUbDoqOAgXEXq6r5SMbTGQN&ipAddress=${
+      document.querySelector("#ipSearch").value
+    }`
+  )
+    .then((res) => res.json())
+    .then(function (data) {
+      result = data;
+    });
+  console.log(result);
+  let coordinates = [result.location.lat, result.location.lng];
+  console.log("coordinates" + coordinates);
+  map_init.panTo(new L.LatLng(...coordinates));
+  dataIP.innerHTML = result.ip;
+  dataTimezone.innerHTML = result.location.timezone + " GMT";
+  dataISP.innerHTML = result.isp;
+  marker.setLatLng(coordinates);
+  addEventListener("resize", resizing);
+  function resizing() {
+    if (window.innerWidth > 1252) {
+      dataLocation.innerHTML = `<span>${result.location.country}</span>  <span>${result.location.city}</span>`;
+    } else {
+      dataLocation.innerHTML = `<div>${result.location.country}</div><br><div>${result.location.city}</div>`;
+    }
+  }
+  resizing();
+  accuracy = 1673.307411212622;
+  let circle = L.circle(coordinates, { radius: accuracy }).addTo(map_init);
+}
 svg.addEventListener("click", function () {
-  map_init.panTo(new L.LatLng(40.737, -73.923));
+  console.log("clicked on SVG");
 });
 
-// map_init = L.map("map", {
-//   center: coordinates,
-//   zoom: 13,
-// });
-
-// map_init.panTo(new L.LatLng(40.737, -73.923));
+svg.addEventListener("click", newSearch);
 
 let reRender = true;
 
 let map_init;
 
-svg.addEventListener("click", function () {});
-document.addEventListener("DOMContentLoaded", entireProject);
-
 // svg.addEventListener("click", map.off());
 
-async function ipSearch() {
-  // await fetch(
-  //   `https://geo.ipify.org/api/v2/country,city?apiKey=at_zVKFWWrUbDoqOAgXEXq6r5SMbTGQN&ipAddress=${
-  //     document.querySelector("#ipSearch").value
-  //   }`
-  // )
-  //   .then((res) => res.json())
-  //   .then(function (data) {
-  //     result = data;
-  //   });
+function ipSearch() {
   dataIP.innerHTML = result.ip;
   dataTimezone.innerHTML = result.location.timezone + " GMT";
   dataISP.innerHTML = result.isp;
@@ -121,10 +139,13 @@ async function ipSearch() {
     center: coordinates,
     zoom: 13,
   });
+  marker = L.marker(coordinates, { alt: "Vilnius" })
+    .addTo(map_init) // "Kyiv" is the accessible name of this marker
+    .bindPopup("you are somewhere here...")
+    .openPopup();
 }
 
 async function entireProject() {
-  // getting user IP address using a different API, therefore making it free
   let userIP = "";
 
   await fetch("https://api64.ipify.org/?format=json", {
@@ -141,27 +162,22 @@ async function entireProject() {
 
   let result;
 
-  await fetch("https://mocki.io/v1/e07f5179-7fd8-4efc-873a-263d681313c6", {
-    headers: { "Content-Type": "application/json" },
-    method: "GET",
-  })
+  let result2 = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_zVKFWWrUbDoqOAgXEXq6r5SMbTGQN&ipAddress=${userIP}`
+  )
     .then((res) => res.json())
     .then(function (data) {
       result = data;
     });
-
-  // this is our limited API that makes all the logic happen
-
-  // await fetch(
-  //   `https://geo.ipify.org/api/v2/country,city?apiKey=at_zVKFWWrUbDoqOAgXEXq6r5SMbTGQN&ipAddress=${userIP}`
-  // )
+  console.log(result2);
+  // await fetch(`https://mocki.io/v1/e07f5179-7fd8-4efc-873a-263d681313c6`, {
+  //   headers: { "Content-Type": "application/json" },
+  //   method: "GET",
+  // })
   //   .then((res) => res.json())
   //   .then(function (data) {
   //     result = data;
   //   });
-
-  // svg.addEventListener("click", apiCall);
-  // apiCall();
 
   dataIP.innerHTML = result.ip;
   dataTimezone.innerHTML = result.location.timezone + " GMT";
@@ -201,7 +217,7 @@ async function entireProject() {
   let circle = L.circle(coordinates, { radius: accuracy }).addTo(map_init);
   // @ViewChild('map') mapContainer;
 
-  let marker = L.marker(coordinates, { alt: "Vilnius" })
+  marker = L.marker(coordinates, { alt: "Vilnius" })
     .addTo(map_init) // "Kyiv" is the accessible name of this marker
     .bindPopup("you are somewhere here...")
     .openPopup();
